2825fcc32fe54241c2ff2d7dd2fc82d4e11393dd
Batın Evirgen <batinevirgen@gmail.com>
2023-05-08 16:39:12 +0300

    sidebar

--- apps/frontend/src/components/ui/DashboardSidebar.tsx
+++ apps/frontend/src/components/ui/DashboardSidebar.tsx
@@ -1,5 +1,6 @@
-import { LayoutDashboardIcon, LucideProps, SettingsIcon, WalletIcon, WorkflowIcon } from 'lucide-react';
+import { LayoutDashboardIcon, LucideProps, SettingsIcon, WalletIcon } from 'lucide-react';
 import Link from 'next/link';
+import { Button } from './Button';
 
 type TDashboardSidebarItemProps = {
   href: string;
@@ -10,12 +11,16 @@ type TDashboardSidebarItemProps = {
 function DashboardSidebarItem({ href, label, icon }: TDashboardSidebarItemProps) {
   return (
     <li>
-      <Link
-        href={href}
-        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-      >
-        {icon}
-        <span className="ml-3">{label}</span>
+      <Link href={href} passHref>
+        <Button
+          className="w-full justify-start text-lg group-hover/all:hover:text-gray-950 group-hover/all:text-gray-400 group/item"
+          variant="ghost"
+        >
+          <span className="[&>*]:mr-3 [&>*]:h-5 [&>*]:w-5 group-hover/item:translate-x-1 transition duration-100">
+            {icon}
+          </span>
+          <span className="group-hover/item:translate-x-2 transition duration-100">{label}</span>
+        </Button>
       </Link>
     </li>
   );
@@ -25,17 +30,17 @@ export default function DashboardSidebar() {
   return (
     <aside
       id="logo-sidebar"
-      className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 md:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+      className="fixed top-0 left-0 z-40 w-64 h-screen pt-16 transition-transform -translate-x-full bg-white border-r border-gray-200 md:translate-x-0"
       aria-label="Sidebar"
     >
-      <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-        <ul className="space-y-2 font-medium">
+      <div className="h-full overflow-y-auto bg-white px-2">
+        <ul className="space-y-2 group/all">
           <DashboardSidebarItem href="/dashboard" label="Dashboard" icon={<LayoutDashboardIcon />} />
-          <DashboardSidebarItem href="/dashboard/pipelines" label="My Pipelines" icon={<WorkflowIcon />} />
           <DashboardSidebarItem href="/dashboard/billing" label="Billing" icon={<WalletIcon />} />
           <DashboardSidebarItem href="/dashboard/settings" label="Settings" icon={<SettingsIcon />} />
         </ul>
       </div>
+      {/* TODO: Add logged in user stuff to bottom */}
     </aside>
   );
 }
