f25ac819991818ae29e3ac242874839e17ec17b5
Batın Evirgen <batinevirgen@gmail.com>
2023-05-15 20:38:58 +0300

    add landing images

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -5,6 +5,7 @@ import Footer from '@/components/landing/Footer';
 import { Logoipsum } from '@/components/landing/Logoipsum';
 import NavigationBar from '@/components/landing/NavigationBar';
 import Pricing from '@/components/landing/Pricing';
+import Image from 'next/image';
 
 const Home = () => {
   return (
@@ -41,9 +42,15 @@ const Home = () => {
 
         <div id="features">
           <div className="grid grid-cols-3 gap-4">
-            <div className="col-span-2 relative h-[512px] overflow-hidden rounded-[64px] hover:rounded-[32px] transition-[border-radius,transform] duration-200">
-              <img src="https://picsum.photos/1024/1024" className="-z-20 w-full h-full absolute inset-0" />
-              <div className="absolute w-full h-full inset-0 bg-gradient-to-t from-black via-black/75 to-transparent -z-10"></div>
+            <div className="col-span-2 relative h-[512px] overflow-hidden rounded-[64px] hover:rounded-[32px] transition-[border-radius,transform] duration-200 border-2 border-gray-300">
+              <Image
+                src="/assets/drag-and-drop.png"
+                width={1333}
+                height={1024}
+                className="-z-20"
+                alt="Image that has Input, Output, Resize, Tint, Watermark and Text nodes that are connected to each other."
+              />
+              <div className="absolute w-full h-full inset-0 bg-gradient-to-t from-black via-black/75 to-transparent"></div>
               <div className="absolute bottom-16 text-center text-white/75 w-3/4 left-1/2 -translate-x-1/2 space-y-4">
                 <h2 className="text-3xl font-medium text-white/90">Drag and Drop</h2>
                 <p className="text-xl">
@@ -51,17 +58,29 @@ const Home = () => {
                 </p>
               </div>
             </div>
-            <div className="relative h-[512px] overflow-hidden rounded-[64px] hover:rounded-[32px] transition-[border-radius,transform] duration-200">
-              <img src="https://picsum.photos/1024/1024" className="-z-20 w-full h-full absolute inset-0" />
-              <div className="absolute w-full h-full inset-0 bg-gradient-to-t from-black via-black/75 to-transparent -z-10"></div>
+            <div className="relative h-[512px] overflow-hidden rounded-[64px] hover:rounded-[32px] transition-[border-radius,transform] duration-200 border-2 border-gray-300">
+              <Image
+                src="/assets/modern-formats.png"
+                width={650}
+                height={1024}
+                className="-z-20"
+                alt="Image that shows output node with it's options. Output node's options are name, format and quality. Format is a selectable input and it has PNG selected, other options are WebP, AVIF, Jpeg, TIFF and GIF."
+              />
+              <div className="absolute w-full h-full inset-0 bg-gradient-to-t from-black via-black/75 to-transparent"></div>
               <div className="absolute bottom-16 text-center text-white/75 w-3/4 left-1/2 -translate-x-1/2 space-y-4">
                 <h2 className="text-3xl font-medium text-white/90">Modern Formats</h2>
                 <p className="text-xl">Output to modern formats such as WebP and AVIF.</p>
               </div>
             </div>
-            <div className="col-span-3 relative h-[512px] overflow-hidden rounded-[64px] hover:rounded-[32px] transition-[border-radius,transform] duration-200">
-              <img src="https://picsum.photos/1024/1024" className="-z-20 w-full h-full absolute inset-0" />
-              <div className="absolute w-full h-full inset-0 bg-gradient-to-br from-black via-black/75 to-transparent -z-10"></div>
+            <div className="col-span-3 relative h-[512px] overflow-hidden rounded-[64px] hover:rounded-[32px] transition-[border-radius,transform] duration-200 border-2 border-gray-300">
+              <Image
+                src="/assets/optimize-and-transform-images.png"
+                width={2016}
+                height={1024}
+                className="-z-20"
+                alt="Image shows nodes that ImgFlow has. Shown nodes are Gamma, Watermark, Flip, Grayscale, Resize, Rotate, Text, Crop, Blur and Modulate."
+              />
+              <div className="absolute w-full h-full inset-0 bg-gradient-to-br from-black via-black/75 to-transparent"></div>
               <div className="absolute top-16 left-16 text-white/75 space-y-4">
                 <h2 className="text-3xl font-medium text-white/90">Optimize and Transform Images</h2>
                 <div>
